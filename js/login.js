e6d5056e207b866c5ab5b98812a1ec8a07d1d4d9
Mohammod Ali <mohammodali651@gmail.com>
2022-08-19 17:22:40 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html'
+    }
+    else{
+        alert('Tui password vule gesos!!! Toke tejjo sontan ghosona korlam')
+    }
+})
